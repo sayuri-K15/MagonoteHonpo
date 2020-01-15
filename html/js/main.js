bba696b5fc4b27d7c62b82151351b56955e8e7a9
sayuri-k15 <zd15shirokuma@gmail.com>
2020-01-15 23:44:43 +0900

    全体レイアウトの最終調整

--- html/js/main.js
+++ html/js/main.js
@@ -1,5 +1,5 @@
 
-jQuery(function() {
+$(function() {
 
 
   //-------------グローバルナビを固定----------
@@ -17,7 +17,22 @@ jQuery(function() {
     }
   })
 
+//---------------スクロールの動き--------------
 
+
+
+  $('a[href^="#"]').click(function(){
+
+      var speed = 700;
+      var href= $(this).attr("href");
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      var position = target.offset().top;
+
+      $("html, body").animate({scrollTop:position}, speed, "swing");
+
+      return false;
+  });
+  
 
   // ------------トップへ戻るボタン------------
   var appear = false;
